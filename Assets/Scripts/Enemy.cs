49dce3373844137b90159534e20bdc7d36ec7473
Paweł Szymkiewicz <szymkiewiczp98@gmail.com>
2021-10-21 00:56:37 +0200

    add enemy movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,14 +7,26 @@ public class Enemy : MonoBehaviour
     public GameObject deathSummon;
     public Vector3 summonPos1, summonPos2;
     public int pointsForKill = 1;
+    public float movementSpeed = 1f;
+    private CharacterController characterController;
+
+    public Vector3 movementDirection;
+    public float remainingMovementTime = 0;
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        
+        characterController.Move(movementDirection * movementSpeed * Time.deltaTime);
+        gameObject.transform.LookAt(gameObject.transform.position + movementDirection);
+        remainingMovementTime -= Time.deltaTime;
+        if (remainingMovementTime <= 0)
+        {
+            remainingMovementTime = Utils.FloatBetween(2, 8);
+            movementDirection = new Vector3(Utils.FloatBetween(-1, 1), 0, Utils.FloatBetween(-1, 1)).normalized;
+        }
     }
 
     public void GetKilled(ScoreSystem system = null)
