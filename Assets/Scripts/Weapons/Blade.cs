e416bd059f508f41287e4b0bad784326c241a9e0
skorafilip <skorafilip@gmail.com>
2021-11-18 11:06:34 +0100

    Blade refactor

--- Assets/Scripts/Weapons/Blade.cs
+++ Assets/Scripts/Weapons/Blade.cs
@@ -4,45 +4,46 @@ using UnityEngine;
 
 public class Blade : MonoBehaviour
 {
-    private GameObject owner;
     public float timeToLive = 10f;
     private float elapsedTime = 0f;
-    private float angle = 0;
 
+    private float startAngle = -45;
+    private float stopAngle = 45;
+    private float angle;
+
+    private float basicSpeed = 15;
     private float speed;
-    // Start is called before the first frame update
+
+    private GameObject owner;
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
         elapsedTime += Time.deltaTime;
         if (elapsedTime > timeToLive)
             Destroy(gameObject);
 
-        var da = 15 * speed * Time.deltaTime;
-        angle += da;
-        if(angle > 90)
+        var dangle = speed * basicSpeed * Time.deltaTime;
+        angle += dangle;
+        if (angle > stopAngle)
             Destroy(gameObject);
 
-        transform.localPosition = Quaternion.Euler(0, da, 0) * gameObject.transform.localPosition;
-
-        var direction = 2 * transform.position - owner.transform.position;
-        direction.y = transform.position.y;
-        transform.LookAt(direction);
+        transform.localPosition = Quaternion.Euler(0, dangle, 0) * transform.localPosition;
+        SetRotation();
     }
 
     public void Launch(GameObject owner, Vector3 direction, float speed)
     {
         this.owner = owner;
         this.speed = speed;
+        this.angle = startAngle;
 
         transform.SetParent(owner.transform);
-        transform.position = transform.position + Quaternion.Euler(0, 360-45, 0) * direction;
-        transform.LookAt(transform.position + Quaternion.Euler(0, 360-45, 0) * direction);
+        transform.position = transform.position + Quaternion.Euler(0, 360 + startAngle, 0) * direction;
+        SetRotation();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -57,4 +58,11 @@ public class Blade : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    private void SetRotation()
+    {
+        var direction = 2 * transform.position - owner.transform.position;
+        direction.y = transform.position.y;
+        transform.LookAt(direction);
+    }
+
 }
