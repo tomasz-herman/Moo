4f0b6dc39a2150e355fdf9f081d7dcc9d937fcb4
Paweł Szymkiewicz <szymkiewiczp98@gmail.com>
2021-12-04 13:56:34 +0100

    Add audio mixer support for volume sliders

--- Assets/Scripts/Menu/OptionsView.cs
+++ Assets/Scripts/Menu/OptionsView.cs
@@ -1,19 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class OptionsView : MenuView
 {
-
+    [SerializeField] private AudioMixer musicMixer, soundMixer;
 
     public void SetEffectsVolume(float volume)
     {
-
+        soundMixer.SetFloat("volume", volume);
     }
 
     public void SetMusicVolume(float volume)
     {
-
+        musicMixer.SetFloat("volume", volume);
     }
 
     public void SetResolution(int resIdx)
