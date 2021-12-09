5756ca4b3a3982385fcab42ab68bcd09ea94d06d
skorafilip <skorafilip@gmail.com>
2021-12-09 12:18:25 +0100

    Added Movement speed Upgrade

---
+++ Assets/Scripts/Upgrades/MovementSpeedUpdate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Upgrades
+{
+    public class MovementSpeedUpdate : UpgradeView
+    {
+        private readonly Player Player;
+        public MovementSpeedUpdate(Player player, Sprite sprite)
+            : base("Movement speed", "Increase movement speed", sprite)
+        {
+            Player = player;
+        }
+
+        public override UpgradeType CommitUpdate()
+        {
+            Player.movement.Speed += 0.2f;
+            return UpgradeType.MaxHealth;
+        }
+    }
+}
