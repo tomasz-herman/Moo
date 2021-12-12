01f365c45d707b8f186609709fc7596172496b61
Michał Rogala <gm.rogala@gmail.com>
2021-12-12 23:57:39 +0100

    Add sound with playback settings class

---
+++ Assets/Scripts/SoundManager/SoundTypeWithPlaybackSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Assets.Scripts.SoundManager
+{
+    [Serializable]
+    public class SoundTypeWithPlaybackSettings
+    {
+        public SoundType SoundType;
+        public PlaybackSettings PlaybackSettings;
+    }
+}
