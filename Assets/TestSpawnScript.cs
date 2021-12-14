1548aa77f4ba3a7e21d7abb0bdf9999893b0e40e
290515 (Dawid Kostrzewa) <01130649@pw.edu.pl>
2021-12-14 14:14:45 +0100

    Add first test procedural generation

---
+++ Assets/TestSpawnScript.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TestSpawnScript : MonoBehaviour
+{
+    [SerializeField] GameObject Player;
+    [SerializeField] List<GameObject> Chambers;
+    [SerializeField] List<GameObject> BossChambers;
+    [SerializeField] List<GameObject> Corridors;
+    [SerializeField] List<GameObject> Enemys;
+    [SerializeField] GameObject Boss;
+    [SerializeField] GameWorld GameWorld;
+    private HashSet<(int x, int y)> taken = new HashSet<(int x, int y)>();
+    private System.Random rng = new System.Random();
+
+
+    void Start()
+    {
+        var StartChamber = Instantiate(Chambers[Mathf.FloorToInt(Random.Range(0, Chambers.Count))], Vector3.zero, Quaternion.identity).GetComponent<Chamber>();
+        taken.Add((0, 0));
+        (int x, int y) current = (0, 0);
+        var en = Instantiate(Enemys[0], StartChamber.SafeSpawnLocation.position, StartChamber.SafeSpawnLocation.rotation);
+        en.GetComponent<Enemy>().deathSummon = null;
+        StartChamber.Enemies.Add(en);
+        Player.transform.position = StartChamber.transform.position + StartChamber.SafeSpawnLocation.position;
+        var exits = StartChamber.getExits();
+
+        CorridorPlacement newCorridorPlacement = null;
+        GameObject newRoom = null;
+        Chamber newChamber = null;
+        var door = exits[FindGoodRandomIndex(exits, current)];
+        exits.Clear();
+        exits.Add(door);
+        for (int i = 0; i < 9; i++)
+        {
+            current = (current.x + (int)door.transform.forward.x, current.y + (int)door.transform.forward.z);
+            newCorridorPlacement = Instantiate(Corridors[Mathf.FloorToInt(Random.Range(0, Corridors.Count))], door.transform.position - door.transform.forward, door.transform.rotation).GetComponent<CorridorPlacement>();
+            if (i % 3 == 2)
+            {
+                newRoom = Instantiate(BossChambers[Mathf.FloorToInt(Random.Range(0, BossChambers.Count))], Vector3.zero, Quaternion.identity);
+                taken.Add((current.x, current.y));
+                taken.Add((current.x, current.y - 1));
+            }
+            else
+            {
+                newRoom = Instantiate(Chambers[Mathf.FloorToInt(Random.Range(0, Chambers.Count))], Vector3.zero, Quaternion.identity);
+                taken.Add((current.x, current.y));
+            }
+            newChamber = newRoom.GetComponent<Chamber>();
+            PlaceChamberOnCorridor(newCorridorPlacement, newRoom, newChamber);
+            exits = newChamber.Exits;
+            door = exits[FindGoodRandomIndex(exits, current)];
+
+            exits.Clear();
+            exits.Add(door);
+        }
+
+        exits.Clear();
+        newChamber.IsLastCHamber = true;
+        newChamber.LastChamberCleared.AddListener(() => { GameWorld.EndGame(true, Player.GetComponent<Player>().scoreSystem.GetScore()); });
+    }
+
+    private void PlaceChamberOnCorridor(CorridorPlacement corridorPlacement, GameObject newRoom, Chamber newChamber)
+    {
+        Door theDoor = null;
+        foreach (var door in newChamber.Exits)
+        {
+            if (door.transform.forward == corridorPlacement.NextDoorTransform.forward)
+                theDoor = door;
+        }
+        Vector3 offset = newRoom.transform.position - theDoor.transform.position;
+        newRoom.transform.position = corridorPlacement.NextDoorTransform.position + offset;
+
+        newChamber.Exits.Remove(theDoor);
+        newChamber.Entrances.Add(theDoor);
+        corridorPlacement.GetComponent<Corridor>().CorridorCenterPassed.AddListener(newChamber.OpenEntrances);
+
+        var en = Instantiate(Enemys[0], newChamber.SafeSpawnLocation.position, newChamber.SafeSpawnLocation.rotation);
+        en.GetComponent<Enemy>().deathSummon = null;
+        newChamber.Enemies.Add(en);
+    }
+
+    private int FindGoodRandomIndex(List<Door> doors, (int x, int y) current)
+    {
+        foreach (int ind in GetRandomOrder(doors.Count))
+        {
+            if (!taken.Contains((current.x + (int)doors[ind].transform.forward.x, current.y + (int)doors[ind].transform.forward.z)))
+                return ind;
+        }
+        return -1;
+    }
+
+    public int[] GetRandomOrder(int numberOfElements)
+    {
+        int[] ind = Enumerable.Range(0, numberOfElements).ToArray();
+
+        int[] RandomInd = ind.OrderBy(x => rng.Next()).ToArray();
+        return RandomInd;
+    }
+
+}
