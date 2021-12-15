43c3d3c0e6fa0c20b52c0db690fc8ae7b4e23e28
Michał Rogala <gm.rogala@gmail.com>
2021-12-15 03:07:07 +0100

    Add SoundTypeSettings to modify volume for specified sound

---
+++ Assets/Scripts/SoundManager/SoundTypeSettings.cs
@@ -0,0 +1,41 @@
+﻿namespace Assets.Scripts.SoundManager
+{
+    public static class SoundTypeSettings
+    {
+        public static float GetVolumeForSoundType(SoundType soundType)
+        {
+            //TODO: this is temporary solution, when we will have all audio clips we can try to normalize them in audacity for example
+            var volume = 1f;
+            switch (soundType)
+            {
+                case SoundType.NoSound:
+                    break;
+                case SoundType.LaserShot:
+                    volume = 0.5f;
+                    break;
+                case SoundType.EnemyKilled:
+                    volume = 1f;
+                    break;
+                case SoundType.GrenadeExplosion:
+                    volume = 0.4f;
+                    break;
+                case SoundType.BackgroundTheme:
+                    break;
+                case SoundType.MusicRoom:
+                    break;
+                case SoundType.Music1:
+                    break;
+                case SoundType.Music2:
+                    break;
+                case SoundType.Music3:
+                    break;
+                case SoundType.Music4:
+                    break;
+                case SoundType.Music5:
+                    break;
+            }
+
+            return volume;
+        }
+    }
+}
