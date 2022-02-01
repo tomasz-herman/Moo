e920f55582126b61b796c309e76d39b2c81821be
Paweł Szymkiewicz <szymkiewiczp98@gmail.com>
2022-02-01 15:57:47 +0100

    Add bossbar visibility toggle

--- Assets/Scripts/UI/BossBar.cs
+++ Assets/Scripts/UI/BossBar.cs
@@ -11,6 +11,17 @@ public class BossBar : MonoBehaviour
 
     private HealthSystem healthSystem;
     private Enemy trackedEnemy;
+    private bool visible;
+
+    public bool Visible
+    {
+        get { return visible; }
+        set
+        {
+            visible = value;
+            CalculateVisibility();
+        }
+    }
 
     public Enemy TrackedEnemy
     {
@@ -20,15 +31,24 @@ public class BossBar : MonoBehaviour
             if (trackedEnemy != null)
             {
                 healthSystem.HealthChanged -= UpdateBar;
+                trackedEnemy.KillEvent.RemoveListener(OnKill);
             }
             trackedEnemy = value;
             if (value != null)
             {
                 healthSystem = trackedEnemy.healthSystem;
                 healthSystem.HealthChanged += UpdateBar;
+                trackedEnemy.KillEvent.AddListener(OnKill);
                 UpdateBar(this, (healthSystem.Health, healthSystem.MaxHealth));
             }
+            CalculateVisibility();
         }
+    }
+
+    private void Start()
+    {
+        Visible = true;
+        CalculateVisibility();
     }
 
     private float timeToSwap = 0;
@@ -42,11 +62,25 @@ public class BossBar : MonoBehaviour
         timeToSwap -= Time.deltaTime;
     }
 
+    private void CalculateVisibility()
+    {
+        bool show = visible && trackedEnemy != null;
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(show);
+        }
+    }
+
     private void UpdateBar(object sender, (float health, float maxHealth) args)
     {
         bar.transform.localScale = new Vector3(args.health / args.maxHealth, 1, 1);
 
         int healthPercent = Mathf.CeilToInt(100 * args.health / args.maxHealth);
         text.text = $"{healthPercent}%";
+    }
+
+    private void OnKill(GameObject obj)
+    {
+        TrackedEnemy = null;
     }
 }
