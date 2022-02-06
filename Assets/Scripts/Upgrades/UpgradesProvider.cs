aff4c8d8123c751fa8e9ead46b33393f086a309a
Michał Rogala <gm.rogala@gmail.com>
2022-02-06 20:00:30 +0100

    Use new one time upgrades in upgrade provider

--- Assets/Scripts/Upgrades/UpgradesProvider.cs
+++ Assets/Scripts/Upgrades/UpgradesProvider.cs
@@ -1,10 +1,7 @@
 using Assets.Scripts.Upgrades;
 using Assets.Scripts.Upgrades.Weapons;
 using System.Collections.Generic;
-using Assets.Scripts.Upgrades.OneTime;
-using Assets.Scripts.Upgrades.OneTime.ProjectileChainsToNearestEnemy;
-using Assets.Scripts.Upgrades.OneTime.ProjectilesExplodeAfterHittingEnemy;
-using Assets.Scripts.Upgrades.OneTime.SwordReflectsEnemyProjectiles;
+using Assets.Scripts.Upgrades.OneTime.SwordReflectsEnemyProjectiles.Upgrades;
 using UnityEngine;
 
 public class UpgradesProvider : MonoBehaviour
@@ -62,10 +59,18 @@ public class UpgradesProvider : MonoBehaviour
         dict.Add(UpgradeType.ShotgunProjectileDispersion, new ShotgunProjectileDispersionUpgrade(shootingSystem.Shotgun, shotgunProjectileDispersionIcon));
 
         //one time upgrades
-        dict.Add(UpgradeType.SwordReflectsEnemyProjectiles, new SwordReflectsEnemyProjectilesUpgrade(shootingSystem, swordReflectsEnemyProjectilesIcon));
-        dict.Add(UpgradeType.ProjectilesExplodeAfterHittingEnemy, new ProjectilesExplodeAfterHittingEnemyUpgrade(shootingSystem, projectilesExplodeAfterHittingEnemyIcon));
-        dict.Add(UpgradeType.ProjectileChainsToNearestEnemy, new ProjectileChainsToNearestEnemyUpgrade(shootingSystem, projectileChainsToNearestEnemy));
+        dict.Add(UpgradeType.SwordReflectsEnemyProjectiles, new SwordReflectsEnemyProjectilesUpgrade(shootingSystem.Sword, swordReflectsEnemyProjectilesIcon));
 
+        //TODO:
+        dict.Add(UpgradeType.PistolProjectilesExplodeAfterHittingEnemy, null/*new ProjectilesExplodeAfterHittingEnemyUpgradeBase(shootingSystem, projectilesExplodeAfterHittingEnemyIcon)*/);
+        dict.Add(UpgradeType.ShotgunProjectilesExplodeAfterHittingEnemy, null);
+        dict.Add(UpgradeType.MachineGunProjectilesExplodeAfterHittingEnemy, null);
+
+        //TODO:
+        dict.Add(UpgradeType.PistolProjectileChainsToNearestEnemy, null/*new ProjectileChainsToNearestEnemyUpgradeBase(shootingSystem, projectileChainsToNearestEnemy*/);
+        dict.Add(UpgradeType.ShotgunProjectileChainsToNearestEnemy, null);
+        dict.Add(UpgradeType.MachineGunProjectileChainsToNearestEnemy, null);
+        dict.Add(UpgradeType.GrenadeLauncherProjectileChainsToNearestEnemy, null);
 
         //rewrite values into array to improve performance
         upgrades = new UpgradeView[dict.Count];
