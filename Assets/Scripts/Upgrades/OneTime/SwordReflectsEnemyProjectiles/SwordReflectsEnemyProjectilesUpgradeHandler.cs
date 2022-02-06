e486f5bc88b9a814c1d94c4ffd3d6a8bc36d25cf
Michał Rogala <gm.rogala@gmail.com>
2022-02-06 16:04:07 +0100

    Make sword reflect projectiles

--- Assets/Scripts/Upgrades/OneTime/SwordReflectsEnemyProjectiles/SwordReflectsEnemyProjectilesUpgradeHandler.cs
+++ Assets/Scripts/Upgrades/OneTime/SwordReflectsEnemyProjectiles/SwordReflectsEnemyProjectilesUpgradeHandler.cs
@@ -1,4 +1,6 @@
-﻿using Assets.Scripts.Upgrades.OneTime.Handlers;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts.Upgrades.OneTime.Handlers;
 using Assets.Scripts.Weapons;
 using UnityEditor;
 using UnityEngine;
@@ -7,26 +9,116 @@ namespace Assets.Scripts.Upgrades.OneTime.SwordReflectsEnemyProjectiles
 {
     public class SwordReflectsEnemyProjectilesUpgradeHandler : IOneTimeProjectileUpgradeHandler
     {
-        //Adjust it to blade length
-        private readonly float _sphereRadius = 2f;
+        //it looks fun when reflected projectile is faster
+        private float _projectileSpeedModifier = 1.5f;
 
-        private readonly Sword _sword;
+        private float _swordLength;
+        private float _swordLengthOver2;
+        private float _swordLengthSquared;
+
         private float _startAngleDeg;
         private float _stopAngleDeg;
         private float _rangeAngleDegOver2;
         private float _cosRangeAngleDegOver2;
-        private float _capsuleRadius;
+        private float _rayCastSphereRadius;
 
-        private GameObject _owner;
+        private Vector3 _bladeDirection;
+        private Vector3 _playerDirection;
+        private Vector3 _rayCastSphereCenter;
 
-        private Vector3 _direction;
-        private Vector3 _cubeCenter;
-        private Vector3 _point0;
-        private Vector3 _point1;
+        private bool _launched;
+
+        private readonly Sword _sword;
+        private readonly HashSet<ProjectileBase> _reflectedProjectiles = new HashSet<ProjectileBase>();
 
         public SwordReflectsEnemyProjectilesUpgradeHandler(Sword sword)
         {
             _sword = sword;
+            if (!_sword.BladeUpgrades.OfType<SwordReflectsEnemyProjectilesUpgradeHandler>().Any())
+            {
+                _sword.BladeUpgrades.Add(this);
+            }
+        }
+
+        public void OnUpdate(ProjectileBase projectile)
+        {
+            if (!_launched) return;
+
+            _bladeDirection = projectile.transform.forward;
+            _playerDirection = projectile.Owner.transform.forward;
+            var position = projectile.transform.position;
+
+            position -= _swordLengthOver2 * _bladeDirection;
+
+            _rayCastSphereCenter = position + _rayCastSphereRadius * _playerDirection;
+
+            var projectileLayer = Layers.GetLayer(Layers.Projectile);
+            int layerMask = 1 << projectileLayer;
+
+            var colliders = Physics.OverlapSphere(_rayCastSphereCenter, _rayCastSphereRadius, layerMask);
+
+            foreach (var collider in colliders)
+            {
+                if (collider.gameObject != projectile.gameObject)
+                {
+                    var offset = collider.transform.position - position;
+                    var distSquared = Vector3.SqrMagnitude(offset);
+
+                    if (distSquared < _swordLengthSquared)
+                    {
+                        var otherProjectile = collider.attachedRigidbody.GetComponent<ProjectileBase>();
+
+                        if (otherProjectile != null)
+                        {
+                            if (!_reflectedProjectiles.Contains(otherProjectile))
+                            {
+                                _reflectedProjectiles.Add(otherProjectile);
+
+                                var direction = otherProjectile.Owner.transform.position - otherProjectile.transform.position;
+
+                                //TODO: do sth with that
+                                direction += Vector3.up;
+
+                                direction.Normalize();
+
+                                otherProjectile.Owner = projectile.Owner;
+
+                                var otherProjectileRigidBody = otherProjectile.gameObject.GetComponent<Rigidbody>();
+                                var otherProjectileSpeed = otherProjectileRigidBody.velocity.magnitude * _projectileSpeedModifier;
+                                var velocity = otherProjectileSpeed * direction;
+                                otherProjectileRigidBody.velocity = velocity;
+                                otherProjectile.gameObject.transform.LookAt(otherProjectile.transform.position + velocity);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void OnDrawGizmos(ProjectileBase projectile)
+        {
+            Gizmos.DrawWireSphere(_rayCastSphereCenter, _rayCastSphereRadius);
+        }
+
+        public void OnLaunch(ProjectileBase projectile)
+        {
+            if (projectile is Blade blade)
+            {
+                _launched = true;
+                _swordLength = blade.length;
+                _swordLengthOver2 = _swordLength / 2f;
+                _swordLengthSquared = _swordLength * _swordLength;
+                _startAngleDeg = blade.startAngle;
+                _stopAngleDeg = blade.stopAngle;
+                _rangeAngleDegOver2 = Mathf.Abs(_startAngleDeg - _stopAngleDeg) / 2f;
+                _cosRangeAngleDegOver2 = Mathf.Cos(_rangeAngleDegOver2);
+
+                _rayCastSphereRadius = _swordLengthOver2 / _cosRangeAngleDegOver2;
+
+                _reflectedProjectiles.Clear();
+
+                OnUpdate(projectile);
+            }
         }
 
         public void OnEnemyHit(ProjectileBase projectile, Enemy enemy)
@@ -37,53 +129,6 @@ namespace Assets.Scripts.Upgrades.OneTime.SwordReflectsEnemyProjectiles
         public void OnTerrainHit(GameObject projectile, Collider terrain)
         {
 
-        }
-
-        public void OnUpdate(ProjectileBase projectile)
-        {
-            _direction = projectile.transform.forward;
-            var position = projectile.transform.position;
-
-            var point0 = position + _capsuleRadius * _direction;
-            var point1 = position + (_sphereRadius -_capsuleRadius) * _direction;
-
-            _point0 = point0;
-            _point1 = point1;
-
-            _cubeCenter = position + 0.5f * _sphereRadius * _direction;
-
-            int layermask = 1 << 10;
-
-            var colliders = Physics.OverlapCapsule(point0, point1, _sphereRadius, layermask);
-
-            foreach (var collider in colliders)
-            {
-                if (collider.gameObject != projectile.gameObject)
-                {
-                    Debug.Log($"W cos walnalem {colliders.Length}");
-                }
-            }
-        }
-
-
-        public void OnDrawGizmos(ProjectileBase projectile)
-        {
-            //Gizmos.DrawCube(_cubeCenter, 0.5f * _sphereRadius * Vector3.one);
-            DrawWireCapsule(_point0, _point1, _sphereRadius);
-        }
-
-        public void OnLaunch(ProjectileBase projectile)
-        {
-            if (projectile is Blade blade)
-            {
-                _startAngleDeg = blade.startAngle;
-                _stopAngleDeg = blade.stopAngle;
-                _rangeAngleDegOver2 = (_startAngleDeg + _stopAngleDeg) / 2f;
-                _cosRangeAngleDegOver2 = Mathf.Cos(_rangeAngleDegOver2);
-
-                _capsuleRadius = _sphereRadius * _cosRangeAngleDegOver2;
-                //_secondRadiusLength = (0.5f * _sphereRadius) / _cosRangeAngleDegOver2;
-            }
         }
 
         public void OnDestroy(ProjectileBase projectile)
@@ -125,61 +170,3 @@ namespace Assets.Scripts.Upgrades.OneTime.SwordReflectsEnemyProjectiles
         }
     }
 }
-
-/*
-interface IUpgradeHandler
-{
-    OnEnemyHit(ProjectileBase projectile, Enemy enemy);
-    OnTerrainHit(GameObject projectile, Collider terrain);
-    OnUpdate(ProjectileBase projectile);
-    OnLaunch(ProjectileBase projectile);
-    // Jakieś inne potrzebne metody?
-}
-
-public abstract class ProjectileBase : Entity
-{
-    public List<IUpgradeHandler> upgradeHandlers = new List<IUpgradeHandler>();
-
-    // inne pola
-
-    protected override void Update()
-    {
-        // to co było
-
-        foreach (IUpgradeHandler handler : upgradeHandlers) handler.OnUpdate(this);
-    }
-
-    public void ApplyDamage(Collider other, float damage)
-    {
-        Enemy enemyHit = other.gameObject.GetComponent<Enemy>();
-        Player playerHit = other.gameObject.GetComponent<Player>();
-        if (Owner != null && Owner.GetComponent<Player>() != null) // Player was shooting
-        {
-            if (enemyHit != null)
-            {
-                enemyHit.TakeDamage(damage, Owner.GetComponent<ScoreSystem>());
-                foreach (IUpgradeHandler handler : upgradeHandlers) handler.OnEnemyHit(this, enemyHit);
-
-            }
-        }
-        if (Owner == null || (Owner != null && Owner.GetComponent<Enemy>() != null)) // Enemy was shooting (if it is null it means it is dead enemy)
-        {
-            if (playerHit != null)
-            {
-                playerHit.healthSystem.Health -= damage;
-            }
-        }
-
-        if //(/*Jakieś wykrywanie czy trafiło w teren/)
-        {
-            foreach (IUpgradeHandler handler : upgradeHandlers) handler.OnTerrainHit(this, other);
-        }
-    }
-
-    // Tą metodę może tu dodać bo jest w granade i bullet:
-    public void Launch(GameObject owner, Vector3 velocity, float extradamage)
-    {
-        foreach (IUpgradeHandler handler : upgradeHandlers) handler.OnLaunch(this);
-    }
-}
-*/
