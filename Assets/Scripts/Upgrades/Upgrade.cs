259f44f3f9a533c45a25c92bf626f4eed9a197ba
Michał Rogala <gm.rogala@gmail.com>
2022-02-06 19:57:31 +0100

    Change UpgradeCount name in Upgrade

--- Assets/Scripts/Upgrades/Upgrade.cs
+++ Assets/Scripts/Upgrades/Upgrade.cs
@@ -1,16 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Upgrade : MonoBehaviour
 {
-    [HideInInspector] public int upgradeCount = 1;
+    [HideInInspector] public int UpgradeCount = 1;
     private void OnTriggerEnter(Collider other)
     {
         Player player = other.gameObject.GetComponent<Player>();
         if(player != null)
         {
-            player.Upgrade(upgradeCount);
+            player.Upgrade(UpgradeCount);
             Destroy(gameObject);
         }
     }
