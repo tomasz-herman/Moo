b069740f158f135c4aba8e115792d8c4bf727261
Michał Rogala <gm.rogala@gmail.com>
2022-02-06 21:26:41 +0100

    Fix name text

--- Assets/Scripts/Upgrades/OneTime/ProjectileChainsToNearestEnemy/Upgrades/ProjectilesChainToNearestEnemyUpgradeBase.cs
+++ Assets/Scripts/Upgrades/OneTime/ProjectileChainsToNearestEnemy/Upgrades/ProjectilesChainToNearestEnemyUpgradeBase.cs
@@ -7,8 +7,8 @@ namespace Assets.Scripts.Upgrades.OneTime.ProjectileChainsToNearestEnemy.Upgrade
     {
         private readonly Weapon _weapon;
         protected ProjectilesChainToNearestEnemyUpgradeBase(Weapon weapon, Sprite sprite, UpgradeType upgradeType)
-            : base($"{Weapon.GetWeaponName(weapon.WeaponType)} Projectile Chaining",
-                $"Projectile chains to nearest enemy after hitting one with {Weapon.GetWeaponName(weapon.WeaponType)}. Chained projectile damage is decreased by 50%.",
+            : base($"{Weapon.GetWeaponName(weapon.WeaponType)} projectile chaining",
+                $"Projectile chains to nearest enemy after hitting one with {Weapon.GetWeaponName(weapon.WeaponType)}. Chained projectile damage is decreased by 50%",
                 sprite, upgradeType)
         {
             _weapon = weapon;
