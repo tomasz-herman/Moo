dd35239fc80cf0a2b091a0f37da2c6013c718a0e
Michał Rogala <gm.rogala@gmail.com>
2022-02-06 19:58:26 +0100

    Add upgrade type field to UpgradeView

--- Assets/Scripts/Upgrades/UpgradeView.cs
+++ Assets/Scripts/Upgrades/UpgradeView.cs
@@ -2,21 +2,22 @@ using UnityEngine;
 
 public abstract class UpgradeView
 {
-    //i think there should be field  upgrade type
-    private string name;
-    private string description;
-    private Sprite sprite;
+    private readonly string _name;
+    private readonly string _description;
+    private readonly Sprite _sprite;
+    public UpgradeType upgradeType { get; protected set; }
 
-    protected UpgradeView(string name, string description, Sprite sprite)
+    protected UpgradeView(string name, string description, Sprite sprite, UpgradeType upgradeType)
     {
-        this.name = name;
-        this.description = description;
-        this.sprite = sprite;
+        this._name = name;
+        this._description = description;
+        this._sprite = sprite;
+        this.upgradeType = upgradeType;
     }
 
-    public string GetName() { return name; }
-    public string GetDescription() { return description; }
-    public Sprite GetSprite() { return sprite; }
+    public string GetName() { return _name; }
+    public string GetDescription() { return _description; }
+    public Sprite GetSprite() { return _sprite; }
 
     public abstract UpgradeType CommitUpdate();
 }
