27c5d264a6ff353cf2d01eac473b882efe70ce0a
Michał Rogala <gm.rogala@gmail.com>
2022-02-06 20:09:04 +0100

    Use updated directory structure for ProjectilesExplodeAfterHittingEnemyUpgrade

---
+++ Assets/Scripts/Upgrades/OneTime/ProjectilesExplodeAfterHittingEnemy/Upgrades/ProjectilesExplodeAfterHittingEnemyUpgradeBase.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Upgrades.OneTime.ProjectilesExplodeAfterHittingEnemy.Upgrades
+{
+    public abstract class ProjectilesExplodeAfterHittingEnemyUpgradeBase : UpgradeView
+    {
+        private readonly Shooting _shootingSystem;
+
+        protected ProjectilesExplodeAfterHittingEnemyUpgradeBase(Shooting shootingSystem, Sprite sprite, UpgradeType upgradeType)
+            : base("Projectile explodes", "Projectile explodes after hitting enemy. Works only on pistol, machine gun and shotgun.", sprite, upgradeType)
+        {
+            _shootingSystem = shootingSystem;
+        }
+    }
+}
