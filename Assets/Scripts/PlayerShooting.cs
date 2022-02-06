f32c52bc8cf7cdf914276d00fcdea30e2ea9d7e2
Tomasz Herman <tomasz.herman@protonmail.com>
2022-02-06 09:48:27 +0100

    Fix weapon scrolling when in UI

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerShooting : MonoBehaviour
 {
@@ -16,12 +17,9 @@ public class PlayerShooting : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && !gameWorld.IsPaused())
-            leftClicked = true;
-        if (Input.GetMouseButtonUp(0))
-            leftClicked = false;
+        if (gameWorld.IsPaused()) return;
 
-        if (leftClicked)
+        if (Input.GetMouseButton((int)MouseButton.LeftMouse))
         {
             shooting.TryShoot(gameObject, gameObject.transform.position + new Vector3(0, 1, 0), gameObject.transform.forward);
         }
