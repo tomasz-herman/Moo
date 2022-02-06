dcad632cc2cb82f6ce0f0afbdc700907b9691ef3
Michał Rogala <gm.rogala@gmail.com>
2022-02-06 21:33:51 +0100

    Add ProjectilesChainToNearestEnemy upgrade classes

---
+++ Assets/Scripts/Upgrades/OneTime/ProjectilesExplodeAfterHittingEnemy/Upgrades/MachineGunProjectilesExplodeAfterHittingEnemyUpgrade.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Scripts.Weapons;
+using UnityEngine;
+
+namespace Assets.Scripts.Upgrades.OneTime.ProjectilesExplodeAfterHittingEnemy.Upgrades
+{
+    public class MachineGunProjectilesExplodeAfterHittingEnemyUpgrade : ProjectilesExplodeAfterHittingEnemyUpgradeBase
+    {
+        public MachineGunProjectilesExplodeAfterHittingEnemyUpgrade(Shotgun shotgun, Sprite sprite)
+            : base(shotgun, sprite, UpgradeType.MachineGunProjectilesExplodeAfterHittingEnemy) {}
+
+        public override UpgradeType CommitUpdate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Scripts/Upgrades/OneTime/ProjectilesExplodeAfterHittingEnemy/Upgrades/PistolProjectilesExplodeAfterHittingEnemyUpgrade.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Scripts.Weapons;
+using UnityEngine;
+
+namespace Assets.Scripts.Upgrades.OneTime.ProjectilesExplodeAfterHittingEnemy.Upgrades
+{
+    public class PistolProjectilesExplodeAfterHittingEnemyUpgrade : ProjectilesExplodeAfterHittingEnemyUpgradeBase
+    {
+        public PistolProjectilesExplodeAfterHittingEnemyUpgrade(Shotgun shotgun, Sprite sprite)
+            : base(shotgun, sprite, UpgradeType.PistolProjectilesExplodeAfterHittingEnemy) {}
+
+        public override UpgradeType CommitUpdate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
