b8b2d6ea3da49a1d1dc499d0092e7bafe0431b80
Paweł Szymkiewicz <szymkiewiczp98@gmail.com>
2022-02-10 17:54:25 +0100

    Add basic ui scaling

--- Assets/Scripts/UI/HeadUpDisplay.cs
+++ Assets/Scripts/UI/HeadUpDisplay.cs
@@ -13,4 +13,18 @@ public class HeadUpDisplay : MonoBehaviour
     public BossBar bossBar;
     public DamagePostProcessing damagePostProcessing;
 
+    public void Awake()
+    {
+        SetHeight(healthBar, ApplicationData.UiData.HealthBarHeight);
+        SetHeight(ammoBar, ApplicationData.UiData.AmmoBarHeight);
+        SetHeight(bossBar, ApplicationData.UiData.BossBarHeight);
+        SetHeight(timerBar, ApplicationData.UiData.TimerBarHeight);
+        SetHeight(scoreBar, ApplicationData.UiData.ScoreBarHeight);
+    }
+
+    private void SetHeight(MonoBehaviour obj, float height)
+    {
+        var rect = obj.GetComponent<RectTransform>();
+        rect.sizeDelta = new Vector2(rect.sizeDelta.x, height);
+    }
 }
