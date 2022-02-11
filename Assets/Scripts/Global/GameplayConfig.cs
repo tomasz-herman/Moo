77fa779410613320a6d1eeef46c52c6aa95849c6
Paweł Szymkiewicz <szymkiewiczp98@gmail.com>
2022-02-11 16:23:05 +0100

    Fix unsaved file

--- Assets/Scripts/Global/GameplayConfig.cs
+++ Assets/Scripts/Global/GameplayConfig.cs
@@ -25,6 +25,7 @@ public class GameplayConfig : ScriptableObject
     public float OptionalChamberClearTime = 40f;
     public float BossChamberClearTime = 60f;
     public float ChamberClearTimeScalingMultiplier = 0.5f;
+    public float QuickWinScoreBonus = 10000;
 
     //https://github.com/tomasz-herman/Moo/projects/6 see third column
     private float GetScalingFactor(int level, float multiplier) { return (float) Math.Pow(1 + ((ScalingFactor - 1) * multiplier), level - 1); }
