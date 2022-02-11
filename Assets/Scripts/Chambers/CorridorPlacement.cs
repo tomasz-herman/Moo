0239e05bff14a4bce67a6392765de45791850d2e
290515 (Dawid Kostrzewa) <01130649@pw.edu.pl>
2022-02-11 19:00:54 +0100

    Some cleanup

--- Assets/Scripts/Chambers/CorridorPlacement.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CorridorPlacement : MonoBehaviour
-{
-    [SerializeField] public Transform NextDoorTransform;
-}
