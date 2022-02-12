4e771917784c1d4cb511f75ae2aa5042d82d7f00
Michał Rogala <gm.rogala@gmail.com>
2022-02-12 21:36:24 +0100

    Fix projectile chain upgrade handler

--- Assets/Scripts/Upgrades/OneTime/ProjectileChainsToNearestEnemy/Handlers/PistolProjectilesChainToNearestEnemyUpgradeHandler.cs
+++ Assets/Scripts/Upgrades/OneTime/ProjectileChainsToNearestEnemy/Handlers/PistolProjectilesChainToNearestEnemyUpgradeHandler.cs
@@ -21,19 +21,12 @@ namespace Assets.Scripts.Upgrades.OneTime.ProjectileChainsToNearestEnemy.Handler
 
         public override void OnEnemyHit(ProjectileBase projectile, Enemy enemy, IOneTimeProjectileUpgradeHandlerData data)
         {
-            //if(!ProjectileDataDictionary.TryGetValue(projectile, out var projectileData)) return;
-            if (!(data is ProjectilesChainToNearestEnemyUpgradeHandlerData projectileData)) return;
-            if (projectileData.HasChained) return;
-
-            projectileData.HasChained = true;
-            //ProjectileDataDictionary[projectile] = projectileData;
-
             var closestEnemy = FindClosestEnemy(enemy);
-            if(closestEnemy == null) return;
+            if (closestEnemy == null) return;
 
             var direction = closestEnemy.transform.position - projectile.transform.position;
             //TODO: do sth with that
-            direction += Vector3.up;
+            direction.y = 0f;
             direction.Normalize();
 
             var projectileRigidBody = projectile.gameObject.GetComponent<Rigidbody>();
@@ -41,9 +34,8 @@ namespace Assets.Scripts.Upgrades.OneTime.ProjectileChainsToNearestEnemy.Handler
             var velocity = projectileSpeed * direction;
 
             Projectile projectileObject = Object.Instantiate(Pistol.projectilePrefab, projectile.transform.position, Quaternion.identity);
-            projectileObject.color = Pistol.color;
+            projectileObject.color = projectile.color;
             projectileObject.name = "ChainedProjectile";
-            //projectileObject.SetUpgrades(Pistol.projectileUpgrades);
             projectileObject.nonCollidableObjects.Add(enemy.gameObject);
             projectileObject.Launch(projectile.Owner, velocity, ChainedProjectileDamageMultiplier * projectile.damage);
         }
