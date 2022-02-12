736068b370b568c5d3112ce9a6d4a60ab677afaa
Michał Rogala <gm.rogala@gmail.com>
2022-02-12 20:19:59 +0100

    Add distance calculation to bullet

--- Assets/Scripts/Weapons/Bullet.cs
+++ Assets/Scripts/Weapons/Bullet.cs
@@ -1,25 +1,30 @@
-using Assets.Scripts.Weapons;
 using UnityEngine;
 
 public class Bullet : Projectile
 {
-    private Vector3 initPosition;
+    private Vector3 lastFramePosition;
+    private float distance;
     private float damageDecayFactor = 0.1f;
+    private float damageOver2;
 
-    protected override void Start()
+    protected override void FixedUpdate()
     {
-        base.Start();
+        base.FixedUpdate();
+        distance += Vector3.Distance(gameObject.transform.position, lastFramePosition);
     }
-    public override void Launch(GameObject owner, Vector3 velocity, float extradamage)
+
+    public override void Launch(GameObject owner, Vector3 velocity, float extraDamage)
     {
-        base.Launch(owner, velocity, extradamage);
-        initPosition = owner.transform.position;
+        base.Launch(owner, velocity, extraDamage);
+        lastFramePosition = owner.transform.position;
+        distance = 0f;
+        damageOver2 = 0.5f * damage;
     }
 
     protected override float CalculateDamage(Collider other)
     {
-        var distance = Vector3.Distance(gameObject.transform.position, initPosition);
         var dmg = (damage) / (1 + damageDecayFactor * distance);
+        dmg = Mathf.Max(dmg, damageOver2);
         return dmg;
     }
 }
