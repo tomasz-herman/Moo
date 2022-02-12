9c24adeb8fa6e59e417642bfee0c87e1bbe9c625
Michał Rogala <gm.rogala@gmail.com>
2022-02-12 20:16:09 +0100

    Add not implemented methods for ProjectilesExplodeAfterHittingEnemyUpgradeHandlerBase

--- Assets/Scripts/Upgrades/OneTime/ProjectilesExplodeAfterHittingEnemy/Handlers/ProjectilesExplodeAfterHittingEnemyUpgradeHandlerBase.cs
+++ Assets/Scripts/Upgrades/OneTime/ProjectilesExplodeAfterHittingEnemy/Handlers/ProjectilesExplodeAfterHittingEnemyUpgradeHandlerBase.cs
@@ -5,34 +5,45 @@ using UnityEngine;
 
 namespace Assets.Scripts.Upgrades.OneTime.ProjectilesExplodeAfterHittingEnemy.Handlers
 {
+    //TODO: add here some probability of explosion
     public class ProjectilesExplodeAfterHittingEnemyUpgradeHandlerBase : IOneTimeProjectileUpgradeHandler
     {
-        public void OnEnemyHit(ProjectileBase projectile, Enemy enemy)
+        public void ApplyUpgrade()
         {
             throw new NotImplementedException();
         }
 
-        public void OnTerrainHit(GameObject projectile, Collider terrain)
+        public void OnEnemyHit(ProjectileBase projectile, Enemy enemy, IOneTimeProjectileUpgradeHandlerData data = null)
         {
             throw new NotImplementedException();
         }
 
-        public void OnUpdate(ProjectileBase projectile)
+        public void OnTerrainHit(GameObject projectile, Collider terrain, IOneTimeProjectileUpgradeHandlerData data = null)
         {
             throw new NotImplementedException();
         }
 
-        public void OnLaunch(ProjectileBase projectile)
+        public void OnUpdate(ProjectileBase projectile, IOneTimeProjectileUpgradeHandlerData data = null)
         {
             throw new NotImplementedException();
         }
 
-        public void OnDestroy(ProjectileBase projectile)
+        public void OnLaunch(ProjectileBase projectile, IOneTimeProjectileUpgradeHandlerData data = null)
         {
             throw new NotImplementedException();
         }
 
-        public void OnDrawGizmos(ProjectileBase projectile)
+        public void OnDestroy(ProjectileBase projectile, IOneTimeProjectileUpgradeHandlerData data = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnDrawGizmos(ProjectileBase projectile, IOneTimeProjectileUpgradeHandlerData data = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IOneTimeProjectileUpgradeHandlerData CreateEmptyData(ProjectileBase projectile)
         {
             throw new NotImplementedException();
         }
