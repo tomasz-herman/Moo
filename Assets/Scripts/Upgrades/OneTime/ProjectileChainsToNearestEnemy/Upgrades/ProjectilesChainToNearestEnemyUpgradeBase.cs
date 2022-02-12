c6b0e2ad5c54f0e57d039d7be3ea89b05fef8a5a
Michał Rogala <gm.rogala@gmail.com>
2022-02-12 20:14:53 +0100

    Add commiting pistol projectil echain upgrade

--- Assets/Scripts/Upgrades/OneTime/ProjectileChainsToNearestEnemy/Upgrades/ProjectilesChainToNearestEnemyUpgradeBase.cs
+++ Assets/Scripts/Upgrades/OneTime/ProjectileChainsToNearestEnemy/Upgrades/ProjectilesChainToNearestEnemyUpgradeBase.cs
@@ -5,13 +5,13 @@ namespace Assets.Scripts.Upgrades.OneTime.ProjectileChainsToNearestEnemy.Upgrade
 {
     public abstract class ProjectilesChainToNearestEnemyUpgradeBase : UpgradeView
     {
-        private readonly Weapon _weapon;
+        protected readonly Weapon Weapon;
         protected ProjectilesChainToNearestEnemyUpgradeBase(Weapon weapon, Sprite sprite, UpgradeType upgradeType)
             : base($"{Weapon.GetWeaponName(weapon.WeaponType)} projectile chaining",
                 $"Projectile chains to nearest enemy after hitting one with {Weapon.GetWeaponName(weapon.WeaponType)}. Chained projectile damage is decreased by 50%",
                 sprite, upgradeType)
         {
-            _weapon = weapon;
+            Weapon = weapon;
         }
     }
 }
