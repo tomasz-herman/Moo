d61ec77c14efe6295304eb4576956f4c50d10d6b
Borys Jedliczko <01110062@pw.edu.pl>
2022-02-13 15:23:36 +0100

    New upgrade model +material + rotation

--- Assets/Scripts/Upgrades/Upgrade.cs
+++ Assets/Scripts/Upgrades/Upgrade.cs
@@ -5,6 +5,19 @@ using UnityEngine;
 public class Upgrade : MonoBehaviour
 {
     [HideInInspector] public int upgradeCount = 1;
+    private new Camera camera;
+
+    private void Start()
+    {
+        camera = Camera.main;
+        transform.Rotate(0, Utils.FloatBetween(0, 360), 0);
+        
+    }
+
+    private void Update()
+    {
+        transform.Rotate(0, Time.deltaTime * 100, 0);
+    }
     private void OnTriggerEnter(Collider other)
     {
         Player player = other.gameObject.GetComponent<Player>();
