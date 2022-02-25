be3357b83e7b63ab5b2837326143b46f4c515992
Paweł Szymkiewicz <szymkiewiczp98@gmail.com>
2022-02-25 03:12:10 +0100

    Fix quality setting resetting bug

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -13,7 +13,7 @@ public class MainMenu : MonoBehaviour
     private OptionsView optionsView;
 
     private MenuView activeView;
-    private bool firstTimeLoading = true;
+    private static bool firstTimeLoading = true;
 
     public void Start()
     {
