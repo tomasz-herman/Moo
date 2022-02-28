acdd4b623720202d42c1b565673df0225db2d3e4
290515 (Dawid Kostrzewa) <01130649@pw.edu.pl>
2022-02-28 17:59:54 +0100

    Clean some code

--- Assets/Scripts/UI/UpgradeWindow.cs
+++ Assets/Scripts/UI/UpgradeWindow.cs
@@ -24,12 +24,12 @@ public class UpgradeWindow : GuiWindow
         upgradeable = FindObjectOfType<Player>();
         Recalculate();
     }
-    
+
 
     public void Recalculate()
     {
         int pendingUpgrades = upgradeable.UpgradeSystem.GetPendingUpgrades();
-        
+
         pendingUpgradesText.text = $"({pendingUpgrades})";
         if (pendingUpgrades == 0)
             pendingUpgradesText.color = noUpgradeColor;
@@ -42,23 +42,18 @@ public class UpgradeWindow : GuiWindow
         rightButton.interactable = interactable;
 
         upgrades = upgradeable.UpgradeSystem.GenerateRandomUpgrades();
-        leftName.text = upgrades[0].GetName();
-        leftName.color = UpgradeColorExtensions.GetColor(upgrades[0].Color);
-        leftDescription.text = upgrades[0].GetDescription(upgradeable);
-        leftButton.image.sprite = upgrades[0].GetSprite();
-        leftButton.image.color = UpgradeColorExtensions.GetColor(upgrades[0].Color);
+        SetUpgradeButon(leftName, leftDescription, leftButton, upgrades[0]);
+        SetUpgradeButon(middleName, middleDescription, middleButton, upgrades[1]);
+        SetUpgradeButon(rightName, rightDescription, rightButton, upgrades[2]);
+    }
 
-        middleName.text = upgrades[1].GetName();
-        middleName.color = UpgradeColorExtensions.GetColor(upgrades[1].Color);
-        middleDescription.text = upgrades[1].GetDescription(upgradeable);
-        middleButton.image.sprite = upgrades[1].GetSprite();
-        middleButton.image.color = UpgradeColorExtensions.GetColor(upgrades[1].Color);
-
-        rightName.text = upgrades[2].GetName();
-        rightName.color = UpgradeColorExtensions.GetColor(upgrades[2].Color);
-        rightDescription.text = upgrades[2].GetDescription(upgradeable);
-        rightButton.image.sprite = upgrades[2].GetSprite();
-        rightButton.image.color = UpgradeColorExtensions.GetColor(upgrades[2].Color);
+    private void SetUpgradeButon(TMP_Text name, TMP_Text description, Button button, UpgradeView upgrade)
+    {
+        name.text = upgrade.GetName();
+        name.color = UpgradeColorExtensions.GetColor(upgrade.Color);
+        description.text = upgrade.GetDescription(upgradeable);
+        button.image.sprite = upgrade.GetSprite();
+        button.image.color = UpgradeColorExtensions.GetColor(upgrade.Color);
     }
 
     private void OnLeftButtonClicked()
