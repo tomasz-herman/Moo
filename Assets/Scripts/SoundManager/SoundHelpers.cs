9e6df8ad7711ea9419be7685012722c4841051e2
Michał Rogala <gm.rogala@gmail.com>
2022-03-02 23:37:57 +0100

    Add new sounds to SoundTypes

--- Assets/Scripts/SoundManager/SoundHelpers.cs
+++ Assets/Scripts/SoundManager/SoundHelpers.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Assets.Scripts.SoundManager
+﻿namespace Assets.Scripts.SoundManager
 {
     public static class SoundHelpers
     {
@@ -39,9 +37,28 @@ namespace Assets.Scripts.SoundManager
 
         public static SoundType GetRandomEnemyDeathSound()
         {
-            //TODO:
-            return SoundType.EnemyKilled;
-            //throw new NotImplementedException();
+            var index = Utils.NumberBetween(0, EnemyDeathSoundTypes.Length - 1);
+            return EnemyDeathSoundTypes[index];
         }
+
+        public static SoundType[] GetEnemyHurtSoundTypes()
+        {
+            return EnemyHurtSoundTypes;
+        }
+
+        public static SoundType[] GetEnemyDeathSoundTypes()
+        {
+            return EnemyDeathSoundTypes;
+        }
+
+        private static readonly SoundType[] EnemyHurtSoundTypes = new SoundType[]
+        {
+            SoundType.enemy_hurt_001, SoundType.enemy_hurt_002, SoundType.enemy_hurt_003, SoundType.enemy_hurt_004, SoundType.enemy_hurt_005, SoundType.enemy_hurt_006, SoundType.enemy_hurt_007, SoundType.enemy_hurt_008, SoundType.enemy_hurt_009, SoundType.enemy_hurt_010, SoundType.enemy_hurt_011, SoundType.enemy_hurt_012, SoundType.enemy_hurt_013, SoundType.enemy_hurt_014, SoundType.enemy_hurt_015, SoundType.enemy_hurt_016, SoundType.enemy_hurt_017, SoundType.enemy_hurt_018, SoundType.enemy_hurt_019, SoundType.enemy_hurt_020, SoundType.enemy_hurt_021, SoundType.enemy_hurt_022, SoundType.enemy_hurt_023, SoundType.enemy_hurt_024, SoundType.enemy_hurt_025, SoundType.enemy_hurt_026, SoundType.enemy_hurt_027, SoundType.enemy_hurt_028, SoundType.enemy_hurt_029, SoundType.enemy_hurt_030, SoundType.enemy_hurt_031, SoundType.enemy_hurt_032
+        };
+
+        private static readonly SoundType[] EnemyDeathSoundTypes = new SoundType[]
+        {
+            SoundType.enemy_die_001, SoundType.enemy_die_002, SoundType.enemy_die_003, SoundType.enemy_die_004, SoundType.enemy_die_005, SoundType.enemy_die_006, SoundType.enemy_die_007, SoundType.enemy_die_008, SoundType.enemy_die_009, SoundType.enemy_die_010, SoundType.enemy_die_011, SoundType.enemy_die_012, SoundType.enemy_die_013, SoundType.enemy_die_014, SoundType.enemy_die_015, SoundType.enemy_die_016, SoundType.enemy_die_017
+        };
     }
 }
--- Assets/Scripts/SoundManager/SoundType.cs
+++ Assets/Scripts/SoundManager/SoundType.cs
@@ -1,4 +1,6 @@
-﻿namespace Assets.Scripts.SoundManager
+﻿// ReSharper disable InconsistentNaming
+
+namespace Assets.Scripts.SoundManager
 {
     public enum SoundType
     {
@@ -11,6 +13,59 @@
         PistolShot = 1004,
         ShotgunShot = 1005,
         SwordSwing = 1006,
+
+        //Enemy die
+        enemy_die_001 = 1011,
+        enemy_die_002 = 1012,
+        enemy_die_003 = 1013,
+        enemy_die_004 = 1014,
+        enemy_die_005 = 1015,
+        enemy_die_006 = 1016,
+        enemy_die_007 = 1017,
+        enemy_die_008 = 1018,
+        enemy_die_009 = 1019,
+        enemy_die_010 = 1020,
+        enemy_die_011 = 1021,
+        enemy_die_012 = 1022,
+        enemy_die_013 = 1023,
+        enemy_die_014 = 1024,
+        enemy_die_015 = 1025,
+        enemy_die_016 = 1026,
+        enemy_die_017 = 1027,
+
+        //Enemy hurt
+        enemy_hurt_001 = 1031,
+        enemy_hurt_002 = 1032,
+        enemy_hurt_003 = 1033,
+        enemy_hurt_004 = 1034,
+        enemy_hurt_005 = 1035,
+        enemy_hurt_006 = 1036,
+        enemy_hurt_007 = 1037,
+        enemy_hurt_008 = 1038,
+        enemy_hurt_009 = 1039,
+        enemy_hurt_010 = 1040,
+        enemy_hurt_011 = 1041,
+        enemy_hurt_012 = 1042,
+        enemy_hurt_013 = 1043,
+        enemy_hurt_014 = 1044,
+        enemy_hurt_015 = 1045,
+        enemy_hurt_016 = 1046,
+        enemy_hurt_017 = 1047,
+        enemy_hurt_018 = 1048,
+        enemy_hurt_019 = 1049,
+        enemy_hurt_020 = 1050,
+        enemy_hurt_021 = 1051,
+        enemy_hurt_022 = 1052,
+        enemy_hurt_023 = 1053,
+        enemy_hurt_024 = 1054,
+        enemy_hurt_025 = 1055,
+        enemy_hurt_026 = 1056,
+        enemy_hurt_027 = 1057,
+        enemy_hurt_028 = 1058,
+        enemy_hurt_029 = 1059,
+        enemy_hurt_030 = 1060,
+        enemy_hurt_031 = 1061,
+        enemy_hurt_032 = 1062,
 
         //UI Sound
         MenuButtonClick = 2001,
