cdabd6f2e25c00fd967327f3bc17e3779307e800
skorafilip <skorafilip@gmail.com>
2022-03-02 19:26:19 +0100

    Review fix + button sounds for quit

--- Assets/Scripts/UI/EndScreen.cs
+++ Assets/Scripts/UI/EndScreen.cs
@@ -14,6 +14,8 @@ public class EndScreen : GuiWindow
 
     [SerializeField] float AnimationTime = 2;
 
+    [SerializeField] string WinText = "You survived!";
+    [SerializeField] string LoseText = "You died!";
 
     private bool Win;
     public void Open(bool win)
@@ -23,25 +25,25 @@ public class EndScreen : GuiWindow
     }
     void Start()
     {
-        panel.transform.localPosition = new Vector3(0, panel.rect.height, 0);
+        panel.transform.localPosition = new Vector3(0, -panel.rect.height, 0);
         if (Win)
         {
-            resultText.text = "You survived!";
+            resultText.text = WinText;
             resultText.color = winColor;
         }
         else
         {
-            resultText.text = "You died!";
+            resultText.text = LoseText;
             resultText.color = loseColor;
         }
     }
     void Update()
     {
-        if (panel.transform.localPosition.y >= 0)
+        if (panel.transform.localPosition.y <= 0)
         {
             var d = Time.fixedDeltaTime / AnimationTime;
-            panel.transform.localPosition = panel.transform.localPosition - Vector3.up * d * panel.rect.height;
-            if (panel.transform.localPosition.y <= 0)
+            panel.transform.localPosition = panel.transform.localPosition + Vector3.up * d * panel.rect.height;
+            if (panel.transform.localPosition.y >= 0)
             {
                 panel.transform.localPosition = Vector3.zero;
             }
